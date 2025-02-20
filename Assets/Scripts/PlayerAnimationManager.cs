b6434bfdcf18ca530d4af107ef8da423bf44763d
Sen B. Donissaint <donissaint13@gmail.com>
2025-02-20 11:20:25 -0500

    HitBox problem
    EnableHitBox
    DisableHitBox

--- Assets/Scripts/PlayerAnimationManager.cs
+++ Assets/Scripts/PlayerAnimationManager.cs
@@ -21,7 +21,7 @@ public class PlayerAnimatorController : MonoBehaviour
     public void Update()
     {
         animator.SetFloat("CharacterSpeed", rb.velocity.magnitude);
-        animator.SetBool("IsGrounded", movement.IsGrounded);
+        animator.SetBool("IsFalling", movement.IsGrounded);
        
         if (Input.GetButtonUp("Fire1"))
         {
